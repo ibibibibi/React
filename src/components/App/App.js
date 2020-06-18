8073244a9a8d54caed72a27056f260021bf28dd8
bibi <baranowskabeata1@gmail.com>
2020-06-18 18:45:35 +0200

    Add List component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import css from './App.scss';
 import Footer from '../Footer/Footer';
+import List from '../List/List';
 
 class App extends React.Component {
   render() {
     return (
       <main className={css.title}>
         <h1>hello</h1>
+        <List />
         <Footer/>
       </main>
     )
---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import css from './List.scss';
+import Footer from '../Footer/Footer';
+
+class List extends React.Component {
+  render() {
+    return (
+      <section className={css.title}>
+        <h2>Lista Lista</h2>
+    </section>
+    )
+  }
+}
+
+export default List;
